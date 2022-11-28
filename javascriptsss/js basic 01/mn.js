4b7bb267ca24ed3d6798c548c805784f825c46c0
Uuganbayr Bymbajaw <byambajawbebe@Uuganbayrs-MacBook-Pro.local>
2022-11-28 15:21:49 +0800

    javascripttt

--- javascriptsss/js basic 01/mn.js
+++ javascriptsss/js basic 01/mn.js
@@ -1,15 +1,6 @@
 var firstName ="Bymbajaw"
 console.log( firstName)
 
-let x=10, y = 4 ;
-var sum = x + y;
-var sub = x - y;
-var mul = x * y;
-var div = x / y;
-var rem = x % y;
-var exp = x ** y;
-console.log("x=",x, "y=",y)
-console.log("nemeh",sum,"hasah",sub,"vrjih", mul,"huwaah", div,"modul", rem,"zereg", exp, )
 
 
 
@@ -66,8 +57,8 @@ console.log("9.", firstName, lastName, age, country)
 
 
 //10
-var MyAge = 18
-var MomAge = 43
+var MyAge = 18;
+var MomAge = 43;
 
 console.log("10.", MomAge)
 
@@ -123,17 +114,276 @@ console.log("14.", BIT)
 
 
 //15
-var mb="15" s="3" br="100000";
-var 
+// var mb="15" s="3" br="100000";
+
 
 
 
 //16
+// S= yzguur door[p(p-a)(p-b)(p-c)]
+// P=a+b+c
+var side1=8, side2=3, side3=3;
+var P = side1 + side2 + side3;
+console.log("16.", P,  "perimetr")
+var s = P * (P-side1) * (P-side2) * (P-side3);
+console.log("16.", s,  "yzguurt oruulna")
+
+var S = Math.sqrt (s);
+console.log("16.", S, "talbai")
+
+
+
+
+
+
+
 //17
+var C = 16, F = 32, a = 1.8;
+var FC = C * a + F;
+console.log("17.", FC)
+
+
+
+
+
+
+
 //18
+var F = 68, a = 0.5555556, C = 32;
+var CF = (F - C) * a; 
+console.log("18.", CF)
+
+
+
+
+
+
+ 
 //19
+let x=10, y = 4 ;
+var sum = x + y;
+var sub = x - y;
+var mul = x * y;
+var div = x / y;
+var rem = x % y;
+var exp = x ** y;
+console.log("19.", "x=",x, "y=",y)
+console.log("19.", "nemeh",sum,"hasah",sub,"vrjih", mul,"huwaah", div,"modul", rem,"zereg", exp, )
+
 //20
+var cm = 16, inch = 0.393701; 
+var INCH = cm * inch;
+console.log("20.", INCH, "inc")
+
+var inch = 160, cm = 2.54 ; 
+var CM = inch * cm;
+console.log("20.", CM, "cm" )
+
+
+
 //21
 //22
+let ex = 24;
+let num = ex.toFixed(1);
+num = ex.toPrecision(1);
+console.log("23.", num)
+
+
+
+
+
+
+
+
 //23
+var baby="Baby Shark"
+var doo="doo-doo"
+var mom="Mommy Shark"
+var dad="Daddy Shark"
+var gma="Grandma Shark"
+var gpa="Grandpa Shark"
+var lets="Let's go hunt"
+var run="Run away"
+var safe="Safe at last"
+var its="It's the end"
+var ah="(ah!)"
+var phew="(phew)"
+
+console.log(
+        baby, doo, doo, 
+        
+)
+console.log(
+    baby, doo, doo, 
+)
+console.log(
+    baby, doo, doo, 
+)
+console.log(
+    baby, doo, doo, 
+)
+console.log(
+    baby 
+)
+  
+
+
+
+console.log(
+    mom, doo, doo, 
+    
+)
+console.log(
+    mom, doo, doo, 
+)
+console.log(
+    mom, doo, doo, 
+)
+console.log(
+    mom, doo, doo, 
+)
+console.log(
+    mom 
+)
+
+
+console.log(
+    dad, doo, doo, 
+    
+)
+console.log(
+    dad, doo, doo, 
+)
+console.log(
+    dad, doo, doo, 
+)
+console.log(
+    dad, doo, doo, 
+)
+console.log(
+    dad 
+)
+
+
+
+console.log(
+    gma, doo, doo, 
+    
+)
+console.log(
+    gma, doo, doo, 
+)
+console.log(
+    gma, doo, doo, 
+)
+console.log(
+    gma, doo, doo, 
+)
+console.log(
+    gma 
+)
+
+
+
+
+console.log(
+    gpa, doo, doo, 
+    
+)
+console.log(
+    gpa, doo, doo, 
+)
+console.log(
+    gpa, doo, doo, 
+)
+console.log(
+    gpa, doo, doo, 
+)
+console.log(
+    gpa 
+)
+
+
+
+console.log(
+    lets, doo, doo, 
+    
+)
+console.log(
+    lets, doo, doo, 
+)
+console.log(
+    lets, doo, doo, 
+)
+console.log(
+    lets, doo, doo, 
+)
+console.log(
+    lets 
+)
+
+
+
+
+console.log(
+    run, doo, doo, 
+    
+)
+console.log(
+    run, doo, doo, 
+)
+console.log(
+    run, doo, doo, 
+)
+console.log(
+    run, doo, doo, 
+)
+console.log(
+    run, ah
+)
+
+
+
+
+console.log(
+    safe, doo, doo, 
+    
+)
+console.log(
+    safe, doo, doo, 
+)
+console.log(
+    safe, doo, doo, 
+)
+console.log(
+    safe, doo, doo, 
+)
+console.log(
+    safe , phew 
+)
+
+
+
+
+
+console.log(
+    its, doo, doo, 
+    
+)
+console.log(
+    its, doo, doo, 
+)
+console.log(
+    its, doo, doo, 
+)
+console.log(
+    its, doo, doo, 
+)
+console.log(
+    its 
+)
+
+
+
+
 //24
