12d790cb3d0bb5ec76c5a21dc47952daa9bec9e6
Morning <morning@mstars-lab3-15s-iMac.local>
2022-12-20 12:39:57 +0800

    adam

---
+++ javascriptsss/PROMISE/onoo.js
@@ -0,0 +1,10 @@
+let taskInput= document.getElementById("new-task");
+
+let addButton = document.getElementById("button")[0];
+
+let incompleteTask = document.getElementById("incomplete-tasks");
+
+
+let createNewTaskElement=function(taskString){
+    
+}
