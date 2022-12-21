1a36a3ff8e629a26add53bcd7e9459aa77b3cd04
Uuganbayr Bymbajaw <byambajawbebe@Uuganbayrs-MacBook-Pro.local>
2022-12-21 10:57:16 +0800

    async

---
+++ javascriptsss/ASYNC AWAIT/my.js
@@ -0,0 +1,11 @@
+console.log("a");
+console.log("b");
+console.log("c");
+for (let i = 0; i < 1_000_000; i++) {
+    for (let i = 0; i < 1_000_000; i++) {
+        
+        
+    }
+}
+
+console.log("final")
